343ac364e2008880b45209655839a6d039780a40
yuki.tasaka <y.tasaka1111@gmail.com>
2023-04-09 17:31:07 +0900

    update

--- public/javascripts/websocket.js
+++ public/javascripts/websocket.js
@@ -1,5 +1,5 @@
 console.log("送信ボタンを押して下さい");
-var sock = new WebSocket('ws://localhost:3011');
+var sock = new WebSocket('ws://express_webhook_deploy_test:3011');
 
 sock.addEventListener('open', function (e) {// 接続
   console.log('Socket 接続成功');
